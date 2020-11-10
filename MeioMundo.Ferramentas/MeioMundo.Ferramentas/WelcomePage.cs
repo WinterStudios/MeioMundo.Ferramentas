cef5066ee9b4ed15ec4eece5d0fcfb2c75998d7f
Hugo Marques <hugo_filipe_marques@live.com.pt>
2020-11-10 18:45:12 +0000

    asa

---
+++ MeioMundo.Ferramentas/MeioMundo.Ferramentas/WelcomePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using U_System.Api.Plugin;
+
+namespace MeioMundo.Ferramentas
+{
+    public class WelcomePage : IPlugin
+    {
+        public string Name { get => "Meio Mundo"; }
+        public string Description { get => "App"; }
+
+        public static string GetInfo() => "It Work";
+    }
+}

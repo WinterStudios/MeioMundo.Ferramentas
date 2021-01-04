7b78cb2109d92ee561f311c7c16957624c786831
Hugo Marques <51680761+WinterStudios@users.noreply.github.com>
2021-01-04 16:33:43 +0000

    v21.1.4-1633.d

---
+++ MeioMundo.Ferramentas/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using U_System.API;
+using U_System.API.Plugins;
+
+namespace MeioMundo.Ferramentas
+{
+    public class EntryPoint : IPlugin
+    {
+        public string Name => "Meio Mundo - Ferramentas";
+        public string Description => "Ferramentas para a Meio Mundo";
+        public SemVersion Version => throw new NotImplementedException();
+        public Module[] Modules => new Module[] { 
+            new Module() {
+                Name = "Codigo de Barras",
+                Type = typeof(UI.Barcode).FullName,
+                Path = "Ferramentas>Codigos de Barras",
+                PluginType = PluginType.Tab
+            }
+        };
+          
+    }
+}

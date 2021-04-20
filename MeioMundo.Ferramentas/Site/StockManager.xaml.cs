51bba6bf775a10d3ceb8c297709b321590710a0e
Hugo Marques <51680761+WinterStudios@users.noreply.github.com>
2021-04-20 21:46:15 +0100

    v

---
+++ MeioMundo.Ferramentas/Site/StockManager.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeioMundo.Ferramentas.Site
+{
+    /// <summary>
+    /// Interaction logic for StockManager.xaml
+    /// </summary>
+    public partial class StockManager : UserControl
+    {
+        public StockManager()
+        {
+            InitializeComponent();
+        }
+    }
+}

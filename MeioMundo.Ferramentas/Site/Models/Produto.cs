5652a73a6f7ed73c87e56642fbaac311327b0f2d
Hugo Marques <51680761+WinterStudios@users.noreply.github.com>
2021-04-21 21:40:48 +0100

    v5

---
+++ MeioMundo.Ferramentas/Site/Models/Produto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeioMundo.Ferramentas.Site.Models
+{
+    public class Produto
+    {
+        public string REF { get; set; }
+        public string Nome { get; set; }
+        public int StockSage { get; set; }
+        public int StockSite { get; set; }
+        public int StockFinal { get => StockSage; }
+        public int StockDiference { get => StockSite - StockSage; }
+        public float Preco_C_IVA { get; set; }
+        public float Preco_S_IVA { 
+            get {
+                if (_iva == TaxaImposto.TaxaNormal)
+                    return (float)(Preco_C_IVA / 1.23);
+                if (_iva == TaxaImposto.TaxaIntermedia)
+                    return (float)(Preco_C_IVA / 1.13);
+                if (_iva == TaxaImposto.TaxaReduzida)
+                    return (float)(Preco_C_IVA / 1.06);
+
+                return 0;
+            } }
+        public string IVA { 
+            get {
+                switch (_iva)
+                {
+                    case TaxaImposto.TaxaReduzida:
+                        return "6 %";
+                    case TaxaImposto.TaxaIntermedia:
+                        return "13 %";
+                    case TaxaImposto.TaxaNormal:
+                        return "23 %";
+                    default:
+                        return "0%";
+                }
+            }
+            set {
+                if (value == "IVA Taxa Normal")
+                    _iva = TaxaImposto.TaxaNormal;
+                if (value == "IVA Taxa Intermedia")
+                    _iva = TaxaImposto.TaxaIntermedia;
+                if (value == "IVA Taxa Reduzida")
+                    _iva = TaxaImposto.TaxaReduzida;
+            }}
+        private TaxaImposto _iva;
+    }
+}
--- MeioMundo.Ferramentas/Site/StockManager.xaml.cs
+++ MeioMundo.Ferramentas/Site/StockManager.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.IO;
+using System.Threading;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +17,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MeioMundo.Ferramentas.Site.Models;
+
 namespace MeioMundo.Ferramentas.Site
 {
     /// <summary>
@@ -20,9 +26,66 @@ namespace MeioMundo.Ferramentas.Site
     /// </summary>
     public partial class StockManager : UserControl
     {
+        public ObservableCollection<Site.Models.Produto> Produtos { get; set; }
         public StockManager()
         {
             InitializeComponent();
+            Produtos = new ObservableCollection<Site.Models.Produto>();
+            UC_StockManager.ItemsSource = Produtos;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Tag = ((Button)sender).Tag.ToString();
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Sage Files|*.xls;*.txt|" +
+                            "Text File (*.txt)|*.txt|" +
+                            "All Files |*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                if(Tag == "Sage")
+                    LoadFile(dialog.FileName, SourceLocation.Sage);
+            }
+                
+            
+        }
+        private async Task LoadFile(string fileLocation, SourceLocation source)
+        {
+            string fileExtension = System.IO.Path.GetExtension(fileLocation);
+            if (fileExtension.ToUpper() == ".TXT" && source == SourceLocation.Sage)
+            {
+                StreamReader reader = new StreamReader(fileLocation);
+                string line;
+                int index = 0;
+                int RefIndex = 0;
+                int ProdutoIndex = 0;
+                int PvpIndex = 0;
+                int IvaIndex = 0;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    string[] cols = line.Split(',');
+                    if(index == 0)
+                    {
+                        RefIndex = cols.ToList().FindIndex(0, cols.Length, x => x == "REF");
+                        ProdutoIndex = cols.ToList().FindIndex(0, cols.Length, x => x == "PRODUTO");
+                        PvpIndex = cols.ToList().FindIndex(0, cols.Length, x => x == "PVP");
+                        IvaIndex = cols.ToList().FindIndex(0, cols.Length, x => x == "IMPOSTO");
+                    }
+                    Produto p = new Produto();
+                    p.REF = cols[RefIndex].Replace("\"", "");
+                    p.Nome = cols[ProdutoIndex].Replace("\"", "");
+                    float preco = float.NaN;
+                    if (float.TryParse(cols[PvpIndex].Replace('.',','), out preco))
+                        p.Preco_C_IVA = preco;
+                    p.IVA = cols[IvaIndex];
+
+                    Produtos.Add(p);
+                    index++;
+                    
+                }
+                
+            }
         }
     }
 }

6e142f60e1c1a28741192fd3c8ab2c0da038be31
Hugo Marques <hugo_filipe_marques@live.com.pt>
2021-10-26 21:00:45 +0100

    v21.10.26-2100

--- MeioMundo.Ferramentas/Internal/CustomScrollViewer.cs
+++ MeioMundo.Ferramentas/Internal/CustomScrollViewer.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace MeioMundo.Ferramentas.Internal
 {
@@ -13,6 +15,15 @@ namespace MeioMundo.Ferramentas.Internal
         //Register a DependencyProperty which has a onChange callback
         public static DependencyProperty CurrentVerticalOffsetProperty = DependencyProperty.Register("CurrentVerticalOffset", typeof(double), typeof(CustomScrollViewer), new PropertyMetadata(new PropertyChangedCallback(OnVerticalChanged)));
         public static DependencyProperty CurrentHorizontalOffsetProperty = DependencyProperty.Register("CurrentHorizontalOffsetOffset", typeof(double), typeof(CustomScrollViewer), new PropertyMetadata(new PropertyChangedCallback(OnHorizontalChanged)));
+
+        Storyboard story = new Storyboard();
+        DoubleAnimation animation = new DoubleAnimation();
+        public CustomScrollViewer()
+        {
+            story.Children.Add(animation);
+            Storyboard.SetTarget(animation, this);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(CustomScrollViewer.CurrentVerticalOffsetProperty));
+        }
 
         //When the DependencyProperty is changed change the vertical offset, thus 'animating' the scrollViewer
         private static void OnVerticalChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -28,6 +39,24 @@ namespace MeioMundo.Ferramentas.Internal
             viewer.ScrollToHorizontalOffset((double)e.NewValue);
         }
 
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            //base.OnMouseWheel(e);
+            double to = e.Delta * -1d;
+            if(to != double.NaN)
+                Scroll(to);
+        }
+
+        private void Scroll(double d)
+        {
+            if (double.IsNaN(d))
+                return;
+            animation.From = this.VerticalOffset;
+            animation.To = CurrentVerticalOffset + d;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(1000));
+            animation.EasingFunction = new ExponentialEase() { EasingMode = EasingMode.EaseOut };
+            story.Begin();
+        }
 
         public double CurrentHorizontalOffset
         {
